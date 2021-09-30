b8862955544624101212e353c1af53e202699c5a
@mrit Bhatt <61137260+amritevon@users.noreply.github.com>
2021-09-30 10:15:51 +0530

    added rabbit mq, communication between services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @EventPattern('eventBus')
+  async helloBus(@Payload() data: any, @Ctx() context: RmqContext) {
+    console.log('=============> helloBus pattern recieved', data);
+    // const channel = context.getChannelRef();
+    // const originalMsg = context.getMessage();
+    return { dataFromEmiiter: data, dataListner: 'listned' };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,16 @@ async function bootstrap() {
   const port = process.env.SERVER_PORT || 5300;
   const app = await NestFactory.create(AppModule);
 
-  // const microservice = app.connectMicroservice({
-  //   transport: Transport.RMQ,
-  //   options: {
-  //     urls: [process.env.RABBIT_URL],
-  //     queue: process.env.RABBIT_QUEUE,
-  //     queueOptions: { durable: true },
-  //   },
-  // });
+  const microservice = app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBIT_URL],
+      queue: process.env.RABBIT_QUEUE,
+      queueOptions: { durable: true },
+    },
+  });
 
-  // await app.startAllMicroservicesAsync();
+  await app.startAllMicroservicesAsync();
 
   await app.listen(port);
   console.log('bridge event bus listening on port : ', port);
